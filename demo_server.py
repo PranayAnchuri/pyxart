3f16dda60d2625d8d4f9aa92124d92133767dd77
Pranay Anchuri <panchuri@princeton.edu>
2023-01-29 19:17:45 -0500

    fixed asyncio warning

--- demo_server.py
+++ demo_server.py
@@ -10,16 +10,18 @@ import grpc
 from rich import print
 from art import text2art
 
+
 async def serve() -> None:
     print(text2art("PYXART Server"))
     server = grpc.aio.server()
     pyxart_pb2_grpc.add_PyxartServicer_to_server(
-        pyxart_servicer.PyxartServicer(), server)
-    server.add_insecure_port('[::]:50051')
+        pyxart_servicer.PyxartServicer(), server
+    )
+    server.add_insecure_port("[::]:50051")
     await server.start()
     await server.wait_for_termination()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    asyncio.get_event_loop().run_until_complete(serve())
+    asyncio.run(serve())
